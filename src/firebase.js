4597a6bfc5aeeb432d0e826d35ecc894024015e6
Muntasir <muntasirenamalhuda@gmail.com>
2021-12-12 21:49:50 +0000

    moved the firebase config codes to env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,16 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyA2OUhI9wMmzZIAxcOvGJU6V9gfy_Ew4-I",
-   authDomain: "connect-me-567b8.firebaseapp.com",
-   databaseURL: "https://connect-me-567b8-default-rtdb.firebaseio.com",
-   projectId: "connect-me-567b8",
-   storageBucket: "connect-me-567b8.appspot.com",
-   messagingSenderId: "506373005424",
-   appId: "1:506373005424:web:549fd3601f7ae4623e14b5",
-   measurementId: "G-54C2W2KNBD"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 // export const auth = firebase.auth();
 // export const firestore = firebase.firestore()
-
-
-
-// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//       databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//          projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//             storageBucket: process.env.REACT_APP_FIREBASE_API_KEY_STORAGE_BUCKET,
-//                messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//                   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//                      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
